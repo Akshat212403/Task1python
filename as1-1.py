b7089a4a461c719d5a3e44a8ab74d9d64fce340f
Akshat212403 <vermaakshat476@gmail.com>
2025-09-14 09:40:00 +0530

    Add files via upload

---
+++ as1-1.py
@@ -0,0 +1,21 @@
+for i in range(1,5): #for multiple check 
+
+    a = input ("enter first number :")
+
+    a=int(a)
+
+    b= input ("enter second number:")
+    b = int(b)
+
+    c = a+b
+    print("addition:",c)
+
+    d =a-b
+    print("subtraction:",d)  
+
+    e = a*b 
+    print("multiplication:",e)
+    f =a/b
+    print("division  :",f)  
+
+    #in additon to this i have used for loop for multiple user inputs  
